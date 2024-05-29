65b28698376658a10e828ed790354e1700b430a3
PebblesDV <pebblesdevries@gmail.com>
2024-05-29 12:10:28 +0200

    seperated header

--- header.php
+++ header.php
@@ -4,17 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vragen</title>
+    <link rel="stylesheet" href="css/question.css">
+    <title>Document</title>
 </head>
 
 <body>
 
-    <div class="question-page">
-
-        <?php include('header.php') ?>
-
-        <div></div>
-        <div></div>
+    <div class="header">
+        <img class="logo" src="public/assets/img/cbr-logo.png" alt="logo">
     </div>
 
 </body>
